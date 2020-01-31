0c76adf552e7d7abcf3cb3cc10cff3238fa98455
Nikki <npeel3497@gmail.com>
2020-01-31 14:30:04 -0500

    Updates

---
+++ precache-manifest.81bb26de735f8ec5a2ffc9d83e94a846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e3fa75ec171e2f0d2ae4f3b71c53dd",
+    "url": "/todolist-app/index.html"
+  },
+  {
+    "revision": "ef99cee6101815ef441c",
+    "url": "/todolist-app/static/css/main.f0a5d828.chunk.css"
+  },
+  {
+    "revision": "b4e7271f955d9370870f",
+    "url": "/todolist-app/static/js/2.1847d0c2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todolist-app/static/js/2.1847d0c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef99cee6101815ef441c",
+    "url": "/todolist-app/static/js/main.9e06f3c9.chunk.js"
+  },
+  {
+    "revision": "1292965a860be8679428",
+    "url": "/todolist-app/static/js/runtime-main.51825114.js"
+  }
+]);
